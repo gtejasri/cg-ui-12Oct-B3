c454f09ab8a81c28cb077cfc2da944bc7c2f87a1
gtejasri <tejusmiley999@gmail.com>
2021-11-30 18:43:35 +0530

    commit

---
+++ 05-my-js.js
@@ -0,0 +1,5 @@
+function anotherFun() {
+    document.write("another fun output by external JS.");
+    document.write('another fun output by external JS.');
+    document.write(`another fun output by external JS.`);
+}
