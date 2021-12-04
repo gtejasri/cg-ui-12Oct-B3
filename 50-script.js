9cc9c315dc8ca4470f493b0ee05524c631fe6bbe
gtejasri <tejusmiley999@gmail.com>
2021-12-04 13:07:22 +0530

    commit

---
+++ 50-script.js
@@ -0,0 +1,21 @@
+
+
+// 51-login.html 
+const loginFunction = () => {
+
+    window.localStorage.setItem("user", document.getElementById("userName").value);
+
+    if (document.getElementById("userName").value === "Sonu"
+        && document.getElementById("password").value === "Sonu") {
+        window.location = "./52-welcome.html";
+    }
+    else {
+        window.location = "./51-login.html";
+        console.log("invalid credentials");
+    }
+}
+
+
+// 52-welcome.html 
+document.getElementById("welcomeUserName").innerHTML =
+    window.localStorage.getItem("user");
